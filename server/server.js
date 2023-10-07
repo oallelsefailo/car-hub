6d9798a339573f6838688b644c1349d40e74fd38
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-10-07 10:04:05 -0700

    added proxy to pkg.json and mod mongo uri text

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(cors(corsOptions));
 app.use("/submit_rides", router);
 
 const dbOptions = { useNewUrlParser: true, useUnifiedTopology: true };
-mongoose.connect(process.env.MONGO_URI, dbOptions);
+mongoose.connect(process.env.mongo_uri, dbOptions);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
