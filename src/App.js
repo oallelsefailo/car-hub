ecbe963cad01d6c20b6304030c007bc05cc7b695
Manny Delgado <mannygdp@gmail.com>
2023-10-07 12:02:29 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import "./App.css";
 import Header from "./components/Header";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
-// import RidesExpanded from "./components/Rides_expanded";
-// import Update from "./components/update"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AboutUs from "./components/aboutUs";
 
@@ -14,10 +12,9 @@ function App() {
       <div className="App">
         <Header />
         <Routes>
-          <Route path="/" element={<Container />} />
-          {/* <Route path="/rides" element={<Rides />} /> */}
-          {/* <Route path="/events" element={<Events />} /> */}
-          <Route path="/aboutUs" element={<AboutUs />} />
+        <Route path="/" element={<Container />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          {/*<Route path="/events" element={<Events />} / */}
         </Routes>
         <Footer />
       </div>
