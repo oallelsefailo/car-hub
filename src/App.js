0178d7a9e6ee6f044e1cdd681f806a73c132e1d0
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-10-08 13:04:27 -0700

    addded rides expanded route to the app.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Rides from "./components/Rides";
 import Update from "./components/update"
 import SubmitRides from "./components/SubmitRides";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RidesExpanded from "./components/Rides_expanded";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/rides" element={<Rides />} />
           <Route path="/rides/update" element={<Update />} />
           <Route path="/rides/submit" element={<SubmitRides />} />
+          <Route path="/rides/:id" element={<RidesExpanded />} />
           {/*<Route path="/events" element={<Events />} />
           <Route path="/about" element={<About />} /> */}
         </Routes>
--- src/components/Rides_expanded.js
+++ src/components/Rides_expanded.js
@@ -4,11 +4,12 @@ import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 function RidesExpanded ({ ride }) {
+    const [rides, setRides] = useState([]);
 
     useEffect(() => {
         const fetchRides = async () => {
             try {
-                const response = await fetch(`http://localhost:5000/submit_rides/${rideId}`);
+                const response = await fetch(`http://localhost:5000/submit_rides/${ride.id}`);
                 if (response.ok) {
                     const data = await response.json();
                     const ridesWithPlaceholders = data.map((ride) => {
@@ -32,6 +33,7 @@ function RidesExpanded ({ ride }) {
     return (
         <div>
             <main>
+            {rides.map((ride) => (
                 <div className="car">
                     <img id="carImg" src={ride.photo} alt={`${ride.brand} ${ride.model}`} />
                     <div className="car-data">
@@ -44,12 +46,12 @@ function RidesExpanded ({ ride }) {
                             <li><b>Owner:</b> {ride.owner}</li>
                         </ul>
                     </div>
-                </div>
+                </div>))}
                 <div className="button">
-                    <button onClick={() => navigate(`/rides/${rideId}/update`)}>
+                    <button onClick={() => navigate(`/rides/${ride.id}/update`)}>
                         Update
                     </button>
-                    <form method="POST" action={`/rides/${rideId}/?_method=DELETE`}>
+                    <form method="POST" action={`/rides/${ride.id}/?_method=DELETE`}>
                     <button>Delete</button>
                     </form>
                 </div>
